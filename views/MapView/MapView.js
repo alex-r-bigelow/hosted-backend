122339855ade70340d2706adbc4e453e766efed0
DevinBayly <baylyd@email.arizona.edu>
2020-04-05 18:21:46 -0700

    get icons and leaflet to show up

--- views/MapView/MapView.js
+++ views/MapView/MapView.js
@@ -1,8 +1,11 @@
 import GoldenLayoutView from '../common/GoldenLayoutView.js';
+import {makeMap} from "./leaflet.js"
 
 class MapView extends GoldenLayoutView {
   constructor (argObj) {
     argObj.resources = [
+      {type:"css",url:"https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"},
+      {type:"js",url:"https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"},
       { type: 'less', url: './views/MapView/style.less' },
       { type: 'text', url: './views/MapView/template.html' }
     ];
@@ -16,8 +19,13 @@ class MapView extends GoldenLayoutView {
   }
   setup () {
     super.setup();
+    // div holding the actual map elements
+    let leafletMap = makeMap()
     // Apply the template; this.content is the div inside the GoldenLayout pane
-    this.content.html(this.resources[1]);
+    this.content.node().append(leafletMap)
+
+
+
 
     // Fill the emptyStateDiv with our warning
     this.emptyStateDiv.html('<h3>TODO: Map</h3>');
---
+++ views/MapView/leaflet.js
@@ -0,0 +1,47 @@
+console.log("loaded")
+
+export let makeMap = () => {
+
+    let mapContainer = document.createElement("div")
+    mapContainer.id = "mapcontainer"
+
+
+    // make the map
+    let map = L.map(mapContainer, {
+        center: [32.253460, -110.911789], // latitude, longitude in decimal degrees (find it on Google Maps with a right click!)
+        zoom: 12, // can be 0-22, higher is closer
+        scrollWheelZoom: true // don't zoom the map on scroll
+    });
+    // add the basemap tiles
+    L.tileLayer(
+        "https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}@2x.png" // stamen toner tiles
+        // stamen toner tiles
+    ).addTo(map);
+
+    // now add the geojson
+
+    let newIcon = L.icon({
+        iconSize: [10, 20],
+        iconUrl: "./views/MapView/hospital_icon.svg",
+    })
+    let markerSetFunc = (gjpt, latlng) => {
+        // substitute the newIcon as the marker
+        return L.marker(latlng, {
+            icon: newIcon
+        })
+    }
+    fetch("./views/MapView/Hospitals.geojson")
+        .then(res => res.json())
+        .then(geojson => {
+
+            //modify the icon used for popup
+            //geojson pt, and latlng[num,num]
+            L.geoJSON(geojson, {
+                pointToLayer: markerSetFunc
+            }).bindPopup(l => {
+                return l.feature.properties.name
+            }).addTo(map)
+
+        })
+    return mapContainer
+}

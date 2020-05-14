e2839ffa1892cc11f2430ee7d08d36a148c1d3c7
Alex Bigelow <alex.r.bigelow@gmail.com>
2020-05-14 12:42:46 -0700

    Support changing table headers

--- views/common/TableViewMixin.js
+++ views/common/TableViewMixin.js
@@ -1,3 +1,5 @@
+/* globals d3 */
+
 const TableViewMixin = function (superclass) {
   const TableView = class extends superclass {
     constructor (argObj) {
@@ -7,12 +9,24 @@ const TableViewMixin = function (superclass) {
         { type: 'text', url: './views/common/TableViewMixinTemplate.html', name: 'TableViewMixinTemplate' }
       ]);
       super(argObj);
+
+      this.visibleHeaders = ['Timestamp'];
     }
     setup () {
       super.setup();
 
       this.content.html(this.getNamedResource('TableViewMixinTemplate'))
         .classed('TableView', true);
+
+      const attributeSelector = this.content.select('.attributeSelector');
+      attributeSelector.on('click', () => {
+        window.controller.tooltip.show({
+          content: tooltipEl => { this.drawAttributeTooltip(tooltipEl); },
+          targetBounds: attributeSelector.node().getBoundingClientRect(),
+          interactive: true,
+          hideAfterMs: 0
+        });
+      });
     }
     draw () {
       super.draw();
@@ -21,18 +35,16 @@ const TableViewMixin = function (superclass) {
         return;
       }
 
-      const headerList = this.getTableHeaders();
-
       this.headers = this.content.select('thead tr')
-        .selectAll('th').data(headerList);
+        .selectAll('th').data(this.visibleHeaders);
       this.headers.exit().remove();
       const headersEnter = this.headers.enter().append('th');
       this.headers = this.headers.merge(headersEnter);
 
-      this.headers.text(d => {
-        return d;
-      });
-      this.headers.attr('title', d => d);
+      headersEnter.append('div');
+      this.headers.select('div')
+        .text(d => d)
+        .attr('title', d => d);
 
       this.rows = this.content.select('tbody')
         .selectAll('tr').data(this.getTableRows())
@@ -42,7 +54,7 @@ const TableViewMixin = function (superclass) {
       this.rows = this.rows.merge(rowsEnter);
 
       this.cells = this.rows.selectAll('td')
-        .data(row => headerList.map(header => row[header]));
+        .data(row => this.visibleHeaders.map(header => row[header]));
       this.cells.exit().remove();
       const cellsEnter = this.cells.enter().append('td');
       this.cells = this.cells.merge(cellsEnter);
@@ -51,6 +63,41 @@ const TableViewMixin = function (superclass) {
       this.cells.select('div')
         .attr('title', d => d === undefined || d === null ? null : d)
         .text(d => d === undefined ? '' : d === null ? 'null' : d);
+    }
+    drawAttributeTooltip (tooltipEl) {
+      const fullHeaderList = this.getTableHeaders();
+
+      tooltipEl.html(`<h3>Show columns:</h3><ul style="padding:0"></ul>`);
+
+      let listItems = tooltipEl.select('ul')
+        .selectAll('li').data(fullHeaderList);
+      listItems.exit().remove();
+      const listItemsEnter = listItems.enter().append('li');
+      listItems = listItems.merge(listItemsEnter);
+
+      listItems
+        .style('max-width', '15em')
+        .style('list-style', 'none')
+        .on('click', () => {
+          d3.event.stopPropagation();
+        });
+
+      listItemsEnter.append('input')
+        .attr('type', 'checkbox')
+        .attr('id', (d, i) => `attrCheckbox${i}`)
+        .property('checked', d => this.visibleHeaders.indexOf(d) !== -1)
+        .on('change', d => {
+          const index = this.visibleHeaders.indexOf(d);
+          if (index === -1) {
+            this.visibleHeaders.push(d);
+          } else {
+            this.visibleHeaders.splice(index, 1);
+          }
+          this.render();
+        });
+      listItemsEnter.append('label')
+        .attr('for', (d, i) => `attrCheckbox${i}`)
+        .text(d => d);
     }
   };
   TableView.prototype._instanceOfTableViewMixin = true;

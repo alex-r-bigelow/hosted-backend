74643da3ffa9e0ead524829a4c66996b1e89629c
DevinBayly <baylyd@email.arizona.edu>
2020-05-23 12:30:48 -0700

    make popupText from visible attributes

--- views/MapView/MapView.js
+++ views/MapView/MapView.js
@@ -2,7 +2,7 @@
 import GoldenLayoutView from '../common/GoldenLayoutView.js';
 
 class MapView extends GoldenLayoutView {
-  constructor (argObj) {
+  constructor(argObj) {
     argObj.resources = [
       { type: 'css', url: 'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css' },
       { type: 'js', url: 'https://unpkg.com/leaflet@1.6.0/dist/leaflet.js' },
@@ -27,10 +27,10 @@ class MapView extends GoldenLayoutView {
     });
     window.controller.houses.on('dataUpdated', () => { this.render(); });
   }
-  get title () {
+  get title() {
     return 'Map';
   }
-  setup () {
+  setup() {
     super.setup();
     // div holding the actual map elements
     const mapContainer = this.content.append('div')
@@ -42,7 +42,7 @@ class MapView extends GoldenLayoutView {
 
     this.setupCustomMapControls();
   }
-  setupCustomMapControls () {
+  setupCustomMapControls() {
     const inputContainer = this.content.insert('div', '#mapcontainer');
     inputContainer.attr('id', 'mapControlHolder');
 
@@ -81,7 +81,7 @@ class MapView extends GoldenLayoutView {
       }
     });
   }
-  makeMap (mapContainer) {
+  makeMap(mapContainer) {
     const map = L.map(mapContainer, {
       center: [32.253460, -110.911789], // latitude, longitude in decimal degrees (find it on Google Maps with a right click!)
       zoom: 12, // can be 0-22, higher is closer
@@ -124,7 +124,7 @@ class MapView extends GoldenLayoutView {
 
     return map;
   }
-  draw () {
+  draw() {
     super.draw();
     if (this.isHidden || this.isLoading) {
       return;
@@ -136,7 +136,7 @@ class MapView extends GoldenLayoutView {
     this.updateZipLayers();
     this.updateCustomMapControls();
   }
-  updateHouseMarkers () {
+  updateHouseMarkers() {
     const blackHouseIcon = L.icon({
       iconSize: [20, 20],
       iconUrl: './views/MapView/house_icon.svg'
@@ -156,14 +156,21 @@ class MapView extends GoldenLayoutView {
           // This house marker already exists, make sure it has a black icon
           this.houseMarkers[markerKey].setIcon(blackHouseIcon);
         } else {
+          //generate popup info
+          // expects visibleAttributes to be a list of columns that are permitted 
+          let popupText = ""
+          if (house.visibleAttributes) {
+            for (let sel of house.visibleAttributes) {
+              popupText+= `<p>${house[sel]}</p>`
+            }
+          } else {
+            popupText = `<p>Property name: ${house["Property name"]}</p>`
+          }
           // Create a new black marker
           this.houseMarkers[markerKey] = L.marker([house.lat, house.lng], {
             icon: blackHouseIcon
           }).addTo(this.leafletMap)
-            .bindPopup(`<p>House Located: ${house['Property Address ']}</p>
-                        <p>Contact: ${house['Primary Contact Name']},
-                          ${house['Primary Contact Email Address']},
-                          ${house['Primary Contact Phone Number']}`)
+            .bindPopup(popupText)
             .on('click', (e) => {
               window.controller.appState.selectHouse(house['Timestamp']);
             });
@@ -177,7 +184,7 @@ class MapView extends GoldenLayoutView {
       }
     }
   }
-  updatePopup () {
+  updatePopup() {
     const houseTimestamp = window.controller.appState.selectedHouseTimestamp;
     if (houseTimestamp && this.houseMarkers[houseTimestamp]) {
       // Selected a house (maybe not from the map); make sure the popup is open
@@ -188,7 +195,7 @@ class MapView extends GoldenLayoutView {
       this.leafletMap.closePopup();
     }
   }
-  updateHospitalCircle () {
+  updateHospitalCircle() {
     const selectedHospital = window.controller.appState.selectedHospital;
     if (selectedHospital) {
       // A hospital is selected; in case it's not the same as before, replace
@@ -212,14 +219,14 @@ class MapView extends GoldenLayoutView {
       }
     }
   }
-  updateZipLayers () {
+  updateZipLayers() {
     const zipNumber = window.controller.appState.selectedZip;
     this.zipGeoJson.eachLayer((layer) => {
       // set or clear the "selected" class
       d3.select(layer._path).classed('selected', zipNumber === layer.feature.properties['ZCTA5CE10']);
     });
   }
-  updateCustomMapControls () {
+  updateCustomMapControls() {
     // Update the hospital radius slider and its label
     this.d3el.select('#hospitalRadiusSlider')
       .property('value', window.controller.appState.hospitalRadius);
